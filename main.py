3d139ad8036701ff1aed4227c551100ec4cff9aa
deniss <denissden1337@gmail.com>
2021-09-23 02:34:44 +0300

    project restructure rewritten README.md demonstration images

--- main.py
+++ main.py
@@ -1,36 +1,133 @@
-import json
-from frame import Frame
+from tkinter import *
+from tkinter import Frame as TkFrame
+from tkinter import filedialog
 from image import KeyboardImage
-from color_layer import Layer
 from functions import *
+from CONSTANTS import  *
+from map import Map
+from export import *
+
+WIDTH, HEIGHT = 850, 400
+IMAGE = "gradient.jpg"
+
+
+class MainWindow(TkFrame):
+    def __init__(self, root):
+        self.root = root
+        super().__init__()
+        self.filename = None
+
+        self.canvas = Canvas(root, width=WIDTH, height=HEIGHT - 100)
+        self.map = Map(KEY_MAP, (WIDTH, HEIGHT - 100))
+        self.image = KeyboardImage(IMAGE)
+        self.keys_list = []
+        self.canvas.pack()
+
+        self.init_window()
 
 
 
-FRAME_INDEX = "frame_selection_range"
-KEYS_AMOUNT = 61
+    def init_window(self):
+        self.master.title("Keyboard Lighting Tool")
+
+        # MENU
+        self.init_menu()
+
+        # KEYBOARD
+        keys_list = []
+        for i in range(len(self.map)):
+            color = rgb_to_hex(self.image[i])
+            k = self.canvas.create_rectangle(self.map[i], fill=color)
+            keys_list.append(k)
+        self.keys_list = keys_list
+
+        # CONTRAST
+        self.constrast_slider = Scale(self.root,
+                                      from_=0, to=300,
+                                      orient=HORIZONTAL,
+                                      length=200,
+                                      label="Contrast",
+                                      command=self.change_constrast)
+        self.constrast_slider.set(100)
+        self.constrast_slider.pack(side=LEFT)
+
+        # HUE
+        self.hue_slider = Scale(self.root,
+                                      from_=0, to=360,
+                                      orient=HORIZONTAL,
+                                      length=200,
+                                      label="Hue",
+                                      command=self.change_hue)
+        self.hue_slider.set(0)
+        self.hue_slider.pack(side=LEFT)
 
 
-with open("test_color.Advanced", "r") as f:
-    ALL_JSON = json.load(f)
+    def init_menu(self):
+        # MENU
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
 
-print(ALL_JSON["pagedata"]["matrix_ColorMode"][0])
-
-colors = KeyboardImage("gradient.jpg").pack()
-
-ALL_JSON["pagedata"]["matrix_ColorMode"] = colors
-
-frames = []
-
-f = Frame()
-f.key_selection = [True] * KEYS_AMOUNT
-frames.append(f.pack())
-
-ALL_JSON["pagedata"]["matrix_frames"] = frames
-ALL_JSON["pagedata"]["currentFramesIndex"] = 60
-ALL_JSON["pagedata"]["projectCode"] = ""
+        # FILE
+        file = Menu(menu)
+        file.add_command(label="Open", command=self.open_image)
+        file.add_command(label="Export", command=self.export_file)
+        file.add_command(label="Exit", command=self.exit_program)
+        menu.add_cascade(label="File", menu=file)
 
 
-print(ALL_JSON)
-input()
-with open("test_result.Advanced", "w+") as f:
-    json.dump(ALL_JSON, f)
+    def change_constrast(self, value):
+        factor = self.constrast_slider.get()
+        self.image.set_contrast(factor / 100)
+        self.update_keyboard()
+
+    def change_hue(self, value):
+        self.image.set_hue(value)
+        self.update_keyboard()
+
+    def update_keyboard(self):
+        for i in range(len(self.image)):
+            color = rgb_to_hex(self.image[i])
+            self.canvas.itemconfig(self.keys_list[i], fill=color)
+
+    def exit_program(self):
+        exit(0)
+
+    def open_image(self):
+        self.filename = filedialog.askopenfilename(initialdir="/",
+                                                        title="Select file",
+                                                        filetypes=(
+                                                            ("jpeg files", "*.jpg"),
+                                                            ("png files", "*.png"),
+                                                            ("all files", "*.*")
+                                                        ))
+        self.image = KeyboardImage(self.filename)
+        self.update_keyboard()
+
+    def export_file(self):
+        save_file = filedialog.asksaveasfilename(initialdir="/",
+                                                 title="Select file",
+                                                 filetypes=(
+                                                    ("APEX Color Profile", "*.Advanced"),
+                                                    ("all files", "*.*")
+                                                    ))
+        export_advanced(save_file, self.image)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app = Tk()
+app.geometry(f"{WIDTH}x{HEIGHT}")
+
+window = MainWindow(app)
+
+app.mainloop()
---
+++ test_files/test_animations.py
@@ -0,0 +1,35 @@
+import json
+from frame import Frame
+from keyboard_image import KeyboardImage
+from layer import Layer
+from functions import *
+
+
+FRAME_INDEX = "frame_selection_range"
+KEYS_AMOUNT = 61
+
+
+with open("test_color.Advanced", "r") as f:
+    ALL_JSON = json.load(f)
+
+print(ALL_JSON["pagedata"]["matrix_ColorMode"][0])
+
+colors = KeyboardImage("gradient.jpg").pack()
+
+ALL_JSON["pagedata"]["matrix_ColorMode"] = colors
+
+frames = []
+
+f = Frame()
+f.key_selection = [True] * KEYS_AMOUNT
+frames.append(f.pack())
+
+ALL_JSON["pagedata"]["matrix_frames"] = frames
+ALL_JSON["pagedata"]["currentFramesIndex"] = 60
+ALL_JSON["pagedata"]["projectCode"] = ""
+
+
+print(ALL_JSON)
+input()
+with open("test_result.Advanced", "w+") as f:
+    json.dump(ALL_JSON, f)
